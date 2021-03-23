35b4eca0cde0496822a1591d46035ade33fab991
mmmfang <mae.fang@gmail.com>
2021-03-23 03:05:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ function total() {
 
 function removeFromCart(item) {
   for (let i=0; i<cart.length; i++) {
-    if (item === cart[i].itemName) {
+    if (cart[i].itemName === item) {
       cart.splice(i, 1);
       return cart;
-    } else {
-      return `That item is not in your cart.`
     }
+      return `That item is not in your cart.`
+    
   }
 }
 
