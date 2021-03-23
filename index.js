e272527043ef40f2f3792b28b2a73f2b530d67cd
mmmfang <mae.fang@gmail.com>
2021-03-23 03:24:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,11 +49,19 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice(i,1)
       return cart;
-    } 
+    }
   } return `That item is not in your cart.`
 }
 
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return `Sorry, we don't have a credit card on file for you.`
+  }
+  if (cardNumber) {
+    let priceTotal = total();
+    let creditCard = cardNumber;
+    cart = [];
+    return `Your total cost is $${priceTotal}, which will be charged to card ${creditCard}. `
+  }
 }
