5d4cb7e2a086df4a9163963351a93160e77e6e3e
mmmfang <mae.fang@gmail.com>
2021-03-23 03:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,16 +43,28 @@ function total() {
     newTotal += cart[i].itemPrice;
   } return newTotal;
 }
+//
+// function removeFromCart(item) {
+//   for (let i=0; i<cart.length; i++) {
+//     if (cart[i].itemName === item) {
+//       cart.splice(i, 1);
+//       return cart;
+//     }
+//       return `That item is not in your cart.`
+//
+//   }
+// }
+
 
 function removeFromCart(item) {
-  for (let i=0; i<cart.length; i++) {
-    if (cart[i].itemName === item) {
-      cart.splice(i, 1);
-      return cart;
-    }
-      return `That item is not in your cart.`
-    
+  // write your code here
+  for (let i = 0; i<cart.length;i++){
+  if (item == cart[i].itemName) {
+    cart.splice(i,1)
+    return cart
   }
+ }
+ return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
