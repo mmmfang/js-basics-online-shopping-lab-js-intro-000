dff17e6099686f1db707e714c827abda964346c8
mmmfang <mae.fang@gmail.com>
2021-03-23 03:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,23 +46,14 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (let i = 0; i<cart.length;i++){
-  if (item == cart[i].itemName) {
+  if (item === cart[i].itemName) {
     cart.splice(i,1)
     return cart
   }
  }
  return "That item is not in your cart."
 }
-// function removeFromCart(item) {
-//   for (let i = 0; i < cart.length; i++) {
-//     if (cart[i].itemName === item) {
-//       cart.splice(i,1)
-//       return cart
-//     } else {
-//       return `That item is not in your cart.`
-//     }
-//   }
-// }
+
 
 
 function placeOrder(cardNumber) {
