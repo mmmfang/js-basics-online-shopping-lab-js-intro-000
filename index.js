9fa86961a0edd9bce68d22f7024f29efbb02b116
mmmfang <mae.fang@gmail.com>
2021-03-23 02:35:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     for (let i=0; i < cart.length; i++) {
       tempArray.push(`${cart[i].itemName} at ${cart[i].itemPrice}`);
       //let ending = ` and ${cart[-1].itemName} at $${cart[-1].itemPrice}.`
-    } return `In your cart, you have ${tempArray.join(`, `)}${ending}.`
+    } return `In your cart, you have ${tempArray.join(`, `)}.`
   }
 }
 
