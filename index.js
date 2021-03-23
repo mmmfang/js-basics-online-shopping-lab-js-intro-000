44ce56c8fc4fe4fc870483c9cfbd08dc30e34c78
mmmfang <mae.fang@gmail.com>
2021-03-23 03:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function viewCart() {
     return `Your shopping cart is empty.`
   } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-  } else if (cart.length === 2) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+//  } else if (cart.length === 2) {
+  //  return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var tempArray=[];
     for (let i=0; i < cart.length; i++) {
