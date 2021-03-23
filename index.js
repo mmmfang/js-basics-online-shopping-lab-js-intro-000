b301a42ecfcfb8fac5e193ec3851926692690109
mmmfang <mae.fang@gmail.com>
2021-03-23 03:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,28 +44,16 @@ function total() {
   } return newTotal;
 }
 //
-// function removeFromCart(item) {
-//   for (let i=0; i<cart.length; i++) {
-//     if (cart[i].itemName === item) {
-//       cart.splice(i, 1);
-//       return cart;
-//     }
-//       return `That item is not in your cart.`
-//
-//   }
-// }
-
-
 function removeFromCart(item) {
-  // write your code here
-  for (let i = 0; i<cart.length;i++){
-  if (item == cart[i].itemName) {
-    cart.splice(i,1)
-    return cart
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1)
+      return cart;
+    }
+      return `That item is not in your cart.`
   }
- }
- return "That item is not in your cart."
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
