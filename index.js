6ac344ffaaf09267a6db89e361e7da06f40e84d3
mmmfang <mae.fang@gmail.com>
2021-03-23 03:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,9 +59,10 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   //if (cardNumber) {
+    cart = [];
     let priceTotal = total();
     //let creditCard = cardNumber;
-    cart = [];
+
     return `Your total cost is $${priceTotal}, which will be charged to the card ${cardNumber}. `
 //  }
 }
